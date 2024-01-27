c9c42dd11a32a9008d1be78f68c8cb3fa08c26ac
priscilla-02 <136003704+priscilla-02@users.noreply.github.com>
2024-01-27 00:05:17 +0000

    feat scroll to top function

--- src/Pages/Home/footer.tsx
+++ src/Pages/Home/footer.tsx
@@ -1,7 +1,33 @@
 export function Footer() {
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+
   return (
-    <div className="flex justify-center items-center font-extrabold h-auto p-10 bg-slate-600">
-      © Copyright 2024. All Rights Reserved.
+    <div
+      className="flex h-auto p-10 bg-slate-600 justify-between items-center"
+      style={{
+        fontFamily: 'rato',
+        fontWeight: '800',
+        fontSize: '20px',
+      }}
+    >
+      <div className="flex items-center">
+        © Copyright 2024. All Rights Reserved.
+      </div>
+
+      <div className="text-black p-2 border-solid border-black border-2 rounded-full w-12 h-12">
+        <button onClick={() => scrollToTop()}>
+          <img
+            src="src/assets/icons/arrow.svg"
+            className="w-10 h-19"
+            alt="Top Arrow Icon"
+          />
+        </button>
+      </div>
     </div>
   );
 }
