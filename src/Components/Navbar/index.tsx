6162a460f29d35a14cfdf19594479d49e0d7ae14
priscilla-02 <136003704+priscilla-02@users.noreply.github.com>
2024-01-27 00:28:47 +0000

    feat nav bar icon change on hover

--- src/Components/Navbar/index.tsx
+++ src/Components/Navbar/index.tsx
@@ -1,77 +1,146 @@
-import { Link } from 'react-router-dom';
 import { useState } from 'react';
+import { DarkModeToggle } from './darkmode';
 
 function Navbar() {
   const [expandNavbar, setExpandNavbar] = useState(false);
+  const [isDarkMode, setIsDarkMode] = useState(false);
+  const [isHomeHovered, setIsHomeHovered] = useState(false);
+  const [isProjectsHovered, setIsProjectsHovered] = useState(false);
+  const [isConnectHovered, setIsConnectHovered] = useState(false);
 
   return (
     <div
-      className={`h-auto block desktop:hidden bg-slate-500 ${
-        expandNavbar ? '' : ''
+      className={`h-auto pb-20  ${expandNavbar ? '' : ''} ${
+        isDarkMode ? 'bg-slate-600' : 'bg-white'
       }`}
     >
-      <div className="w-full h-24 flex items-center justify-end">
-        <button
-          onClick={() => {
-            setExpandNavbar(prev => !prev);
-          }}
-          className="mr-5 cursor-pointer"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            className="w-6 h-6"
+      <div className="w-full h-24 flex items-center justify-between">
+        <div className="ml-8 py-3 h-18 align-middle select-none transition duration-200 ease-in">
+          <button
+            onClick={() => {
+              setIsDarkMode(prev => !prev);
+            }}
           >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+            <DarkModeToggle
+              isDarkMode={isDarkMode}
+              setIsDarkMode={setIsDarkMode}
             />
-          </svg>
-        </button>
+          </button>
+        </div>
+
+        <div className="mr-8">
+          <button
+            onClick={() => {
+              setExpandNavbar(prev => !prev);
+            }}
+            className="mr-5 cursor-pointer block desktop:hidden"
+          >
+            <div className="w-10 h-10">
+              <img src="src/assets/icons/menu.svg" alt="Menu Icon" />
+            </div>
+          </button>
+        </div>
       </div>
 
       <div
-        className={`w-full h-full flex items-center justify-center  ${
+        className={`w-full h-full my-20 flex items-center justify-center  ${
           expandNavbar ? 'flex-col justify-start mt-24' : 'hidden'
         }`}
       >
-        <Link
-          className={`w-full text-center ${
-            expandNavbar ? 'w-full text-center m-8' : 'hidden'
-          }`}
-          to="/"
-          onClick={() => {
-            setExpandNavbar(false);
-          }}
-        >
-          Home
-        </Link>
-        <Link
-          className={`w-full text-center ${
-            expandNavbar ? 'w-full text-center m-8' : 'hidden'
-          }`}
-          to="/projects"
-          onClick={() => {
-            setExpandNavbar(false);
-          }}
-        >
-          Projects
-        </Link>
-        <Link
-          className={`w-full text-center ${
-            expandNavbar ? 'w-full text-center m-8' : 'hidden'
-          }`}
-          to="/contact"
-          onClick={() => {
-            setExpandNavbar(false);
-          }}
-        >
-          Contact Me
-        </Link>
+        <div>
+          <div className="flex-col">
+            <div
+              className="flex justify-center items-center align-center"
+              onMouseEnter={() => setIsHomeHovered(true)}
+              onMouseLeave={() => setIsHomeHovered(false)}
+            >
+              <img
+                src={`src/assets/icons/home-${
+                  isHomeHovered ? 'solid' : 'outline'
+                }.svg`}
+                alt="Home Icon"
+                className="w-12 h-12"
+              />
+
+              <a
+                href="#aboutme"
+                className={`w-full text-center  ${
+                  expandNavbar ? 'w-full text-center m-8' : 'hidden'
+                }`}
+                onClick={() => {
+                  setExpandNavbar(false);
+                }}
+                style={{
+                  fontFamily: 'rato',
+                  fontWeight: '800',
+                  fontSize: '30px',
+                }}
+              >
+                Home
+              </a>
+            </div>
+            <div
+              className="flex justify-center items-center align-center"
+              onMouseEnter={() => setIsProjectsHovered(true)}
+              onMouseLeave={() => setIsProjectsHovered(false)}
+            >
+              <img
+                src={`src/assets/icons/projects-${
+                  isProjectsHovered ? 'solid' : 'outline'
+                }.svg`}
+                alt="Project Icon"
+                className="w-12 h-12"
+              />
+
+              <a
+                href="#projects"
+                className={`w-full text-center  ${
+                  expandNavbar ? 'w-full text-center m-8' : 'hidden'
+                }`}
+                onClick={() => {
+                  setExpandNavbar(false);
+                }}
+                style={{
+                  fontFamily: 'rato',
+                  fontWeight: '800',
+                  fontSize: '30px',
+                }}
+              >
+                Projects
+              </a>
+            </div>
+            <div
+              className="flex justify-center items-center align-center"
+              onMouseEnter={() => setIsConnectHovered(true)}
+              onMouseLeave={() => setIsConnectHovered(false)}
+            >
+              <img
+                src={`src/assets/icons/projects-${
+                  isConnectHovered ? 'solid' : 'outline'
+                }.svg`}
+                className="w-12 h-12"
+                alt="Connect Icon"
+              />
+
+              <a
+                href="#contact"
+                className={`w-full text-center  ${
+                  expandNavbar ? 'w-full text-center m-8' : 'hidden'
+                }`}
+                onClick={() => {
+                  setExpandNavbar(false);
+                }}
+                style={{
+                  fontFamily: 'rato',
+                  fontWeight: '800',
+                  fontSize: '30px',
+                }}
+              >
+                Connect with Me
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
