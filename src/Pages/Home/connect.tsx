637ab7766d11bfaea67ef0680cb653f3b625be7a
priscilla-02 <136003704+priscilla-02@users.noreply.github.com>
2024-01-29 00:25:18 +0000

    edit connect info

--- src/Pages/Home/connect.tsx
+++ src/Pages/Home/connect.tsx
@@ -1,10 +1,83 @@
+import { useState } from 'react';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+
 export function Connect() {
+  const [isHovered, setIsHovered] = useState<null | string>(null);
+
+  const handleIconEnter = (iconName: string) => {
+    setIsHovered(iconName);
+  };
+
+  const handleIconLeave = () => {
+    setIsHovered(null);
+  };
+
   return (
     <div
       id="connect"
-      className="flex justify-center items-center font-extrabold h-[300px]"
+      className="flex justify-center items-center font-extrabold h-[500px]"
     >
-      Contact me!
+      <div className="flex-col">
+        <p
+          className="mb-5 text-3xl"
+          style={{
+            fontFamily: 'rato',
+            fontWeight: '800',
+          }}
+        >
+          Connect with me here:
+        </p>
+        <div className="flex items-center align-center justify-center">
+          <div
+            className={`mx-7 ${
+              isHovered === 'github' ? 'rotate-out' : 'rotate-in'
+            }`}
+            onMouseEnter={() => handleIconEnter('github')}
+            onMouseLeave={handleIconLeave}
+            style={{
+              position: 'relative',
+            }}
+          >
+            <a href="https://github.com/priscilla-02" target="_blank">
+              <div
+                className={`rounded-full border-2 border-gray-700 p-3.5 ${
+                  isHovered === 'github'
+                    ? 'ring-inset ring-[6px] ring-blue-400'
+                    : ''
+                }`}
+              >
+                <GitHubIcon style={{ fontSize: 70 }} />
+              </div>
+            </a>
+          </div>
+          <div
+            className={`mx-5 ${
+              isHovered === 'linkedin' ? 'rotate-out' : 'rotate-in'
+            }`}
+            onMouseEnter={() => handleIconEnter('linkedin')}
+            onMouseLeave={handleIconLeave}
+            style={{
+              position: 'relative',
+            }}
+          >
+            <a
+              href="https://www.linkedin.com/in/priscilla-chan-554153138/"
+              target="_blank"
+            >
+              <div
+                className={`rounded-full border-2 border-gray-700 p-2 ${
+                  isHovered === 'linkedin'
+                    ? 'ring-inset ring-[6px] ring-blue-400'
+                    : ''
+                }`}
+              >
+                <LinkedInIcon style={{ fontSize: 80 }} />
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
