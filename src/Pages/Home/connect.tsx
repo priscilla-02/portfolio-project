f4640d6c651deb399cdb3370e4905018b1c65f97
priscilla-02 <136003704+priscilla-02@users.noreply.github.com>
2024-02-01 23:19:50 +0000

    add link to email and onTouch

--- src/Pages/Home/connect.tsx
+++ src/Pages/Home/connect.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { useTheme } from '../../Hooks/themeHook';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import EmailIcon from '@mui/icons-material/Email';
 
 export function Connect() {
   const [isHovered, setIsHovered] = useState<null | string>(null);
@@ -39,6 +40,8 @@ export function Connect() {
             }`}
             onMouseEnter={() => handleIconEnter('github')}
             onMouseLeave={handleIconLeave}
+            onTouchStart={() => handleIconEnter('github')}
+            onTouchEnd={handleIconLeave}
             style={{
               position: 'relative',
             }}
@@ -51,7 +54,7 @@ export function Connect() {
                     : ''
                 }`}
               >
-                <GitHubIcon style={{ fontSize: 35 }} />
+                <GitHubIcon style={{ fontSize: 25 }} />
               </div>
             </a>
           </div>
@@ -61,6 +64,8 @@ export function Connect() {
             }`}
             onMouseEnter={() => handleIconEnter('linkedin')}
             onMouseLeave={handleIconLeave}
+            onTouchStart={() => handleIconEnter('linkedin')}
+            onTouchEnd={handleIconLeave}
             style={{
               position: 'relative',
             }}
@@ -76,7 +81,32 @@ export function Connect() {
                     : ''
                 }`}
               >
-                <LinkedInIcon style={{ fontSize: 45 }} />
+                <LinkedInIcon style={{ fontSize: 35 }} />
+              </div>
+            </a>
+          </div>
+
+          <div
+            className={`mx-5 ${
+              isHovered === 'email' ? 'rotate-out' : 'rotate-in'
+            }`}
+            onMouseEnter={() => handleIconEnter('email')}
+            onMouseLeave={handleIconLeave}
+            onTouchStart={() => handleIconEnter('email')}
+            onTouchEnd={handleIconLeave}
+            style={{
+              position: 'relative',
+            }}
+          >
+            <a href="mailto:priscilla.chankwaitung@gmail.com" target="_blank">
+              <div
+                className={`rounded-full border-2 border-gray-700 p-2 ${
+                  isHovered === 'email'
+                    ? 'ring-inset ring-[6px] ring-blue-400'
+                    : ''
+                }`}
+              >
+                <EmailIcon style={{ fontSize: 35 }} />
               </div>
             </a>
           </div>
